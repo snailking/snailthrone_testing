05c97a56ce4b292158cfa42b9aa69a347d619464
snailking <39387494+snailking@users.noreply.github.com>
2018-11-14 22:31:54 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -32,6 +32,39 @@ var f_sacrifice = 40;
 var m_account = "";
 var n_account = "";
 
+/* MODAL */
+
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the button that opens the modal
+var b_helpmodal = document.getElementById("helpmodal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+b_helpmodal.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Close modal on game info
+function closeModal() {
+	modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 /* GLOBAL LOOP */
 
 //Started once, to trigger the main loop and the egg loop
