f5dd48cdb3f99fff4362c3ffa79f8d236ba85208
SnailKing <39387494+snailking@users.noreply.github.com>
2019-01-06 09:29:08 +0100

    Update snailthrone.js

--- snailthrone.js
+++ snailthrone.js
@@ -330,7 +330,7 @@ function updateGodPot(){
 	var godpotdoc = document.getElementById('godpot');
 	console.log("updating god pot...");
 	godPot(function(req) {
-		console.log("godpot is " + formatEthValue(web3.fromWei(req,'ether'));
+		console.log("godpot is " + formatEthValue(web3.fromWei(req,'ether')));
 		a_godPot = formatEthValue(web3.fromWei(req,'ether'));
 		godpotdoc.textContent = a_godPot;
 	});
